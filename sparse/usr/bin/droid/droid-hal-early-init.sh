3386f0ab99d19eaa92f75caae34546e1fe7aed2e
edp17 <edp17@pm.me>
2024-10-07 11:29:22 +0100

    Fixed Audio!!

--- sparse/usr/bin/droid/droid-hal-early-init.sh
+++ sparse/usr/bin/droid/droid-hal-early-init.sh
@@ -5,5 +5,4 @@ if ! grep -q hybris /system/etc/ld.config.29.txt; then
 fi
 
 # Fix audio with audio.hidl_compat
-mount -o bind /usr/libexec/droid-hybris/system/lib64/hw/audio.hidl_compat.default.so /vendor/lib64/hw/audio.primary.default.so
-mount -o bind /usr/libexec/droid-hybris/system/lib/hw/audio.hidl_compat.default.so /vendor/lib/hw/audio.primary.default.so
+mount -o bind /usr/libexec/droid-hybris/system/lib64/hw/audio.hidl_compat.default.so /vendor/lib64/hw/audio.primary.msm8953.so
